a7a5bff91aadfce6d1bb7ed2645b50cc5b31f2fc
manolors <manolo.rs2005@gmail.com>
2023-06-08 19:43:39 +0100

    add validations

--- netlify/functions/suma/suma.js
+++ netlify/functions/suma/suma.js
@@ -1,18 +1,26 @@
+function isNumeric(str) {
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
 const handler = async (event) => {
   try {
+    const num1 = event.queryStringParameters.num1 || false;
+    const num2 = event.queryStringParameters.num2 || false;
 
-    const num1 = parseInt(event.queryStringParameters.num1) || 0
-    const num2 = parseInt(event.queryStringParameters.num2) || 0
-
-    const resultado = num1 + num2
+    if (!num1 || !num2 || !isNumeric(num1) || !isNumeric(num2)) {
+      return {
+        statusCode: 400,
+        body: "Please provide valid num1 and num2!!!",
+      };
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: resultado }),
-    }
+      body: JSON.stringify({ suma: parseInt(num1) + parseInt(num2) }),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
