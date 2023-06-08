9e040b497b375bff40452e1f41c1074e9168f38d
manolors <manolo.rs2005@gmail.com>
2023-06-08 19:33:53 +0100

    add parseint

--- netlify/functions/suma/suma.js
+++ netlify/functions/suma/suma.js
@@ -3,11 +3,12 @@ const handler = async (event) => {
 
     const num1 = parseInt(event.queryStringParameters.num1) || 0
     const num2 = parseInt(event.queryStringParameters.num2) || 0
+
     const resultado = num1 + num2
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: resultado}),
+      body: JSON.stringify({ message: resultado }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
